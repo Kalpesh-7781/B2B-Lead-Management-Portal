646e7f21750ed7a34e4a11de580a7cb5249a8cd6
Kalpesh Mewada <Kalpesh Mewada@DESKTOP-SGH9SNI>
2024-02-21 20:50:11 +0530

    Updated

--- src/main/java/com/services/LeadServiceImpl.java
+++ src/main/java/com/services/LeadServiceImpl.java
@@ -16,13 +16,7 @@ public class LeadServiceImpl implements LeadService {
     	Lead existingLead = leadRepository.findByEmailAndCampaign(lead.getEmail(), lead.getCampaign());
 
         if (existingLead != null) {
-            // Handle duplicate based on requirements:
-            // - Log a warning
-            //log.warn("Duplicate lead found: email={}, campaign={}", lead.getEmail(), lead.getCampaign());
-            // - Update existing lead information (if applicable)
-            //   existingLead.updateWithNewData(lead);
-            //   return existingLead;
-            // - Throw an exception (for strict duplicate prevention)
+            
             throw new IllegalArgumentException("Duplicate email already exists for the same campaign ID");
         }
     	
